92aa678fb9b9eebd1c8a1b370f0b68cdf7bbf176
zjut-chenkai <1527285544@qq.com>
2021-09-23 12:37:32 +0800

    测试
    测试

--- springcloud-webservice/src/main/java/org/com/ck/SpringContext.java
+++ springcloud-webservice/src/main/java/org/com/ck/SpringContext.java
@@ -6,14 +6,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-/**
- * 
- * 
- * @author zhuqun
- * @date 2020年1月15日 - 下午3:02:02
- * @history
- *          2020年1月15日 - 下午3:02:02 zhuqun create.
- */
+
 @Component
 @Lazy(false)
 public class SpringContext implements ApplicationContextAware {
