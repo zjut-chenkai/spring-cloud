7e44da4c87b7f6cb4a473d746b2860e0b46bc363
zjut-chenkai <1527285544@qq.com>
2021-09-23 12:37:03 +0800

    测试
    测试

--- springcloud-webservice/src/main/java/org/com/ck/SpringBootMain.java
+++ springcloud-webservice/src/main/java/org/com/ck/SpringBootMain.java
@@ -5,14 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
- * 
- * 
- * @author zhuqun
- * @date 2020年1月15日 - 下午3:02:43
- * @history
- *          2020年1月15日 - 下午3:02:43 zhuqun create.
- */
+
 @SpringBootApplication
 public class SpringBootMain {
     public static void main(final String[] args) {
