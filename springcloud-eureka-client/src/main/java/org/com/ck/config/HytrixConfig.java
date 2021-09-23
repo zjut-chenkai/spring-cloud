3e3b2bd3eb126b905ba17555bfee8f2ce799eb17
zjut-chenkai <1527285544@qq.com>
2021-09-23 12:29:29 +0800

    测试
    测试

--- springcloud-eureka-client/src/main/java/org/com/ck/config/HytrixConfig.java
+++ springcloud-eureka-client/src/main/java/org/com/ck/config/HytrixConfig.java
@@ -1,11 +1,4 @@
-/**
- * @ spring-cloud-eureka-hystrix-consumer
- * @ com.cmcc.dhome.config
- * @ HytrixConfig.java
- * 
- * @author Shi Junjian  2018年10月31日
- * Copyright © CMCC Corporation. All rights reserved.
- */
+
 package org.com.ck.config;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -14,13 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
-/**
- * springboot2.0,必须添加此配置，否则hytrix dashboard无法监控到数据
- * @author Shi Junjian 
- * @date 2018年10月31日 - 下午3:31:52
- * @history 
- * 		 2018年10月31日 - 下午3:31:52 Shi Junjian  create.
- */
+
 @Configuration
 public class HytrixConfig {
     @Bean
